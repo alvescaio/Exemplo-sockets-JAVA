881c10e138752f8b7fee3ddc94a995916d5c480a
alvescaio <caio_alves@outlook.com>
2017-01-07 20:15:40 -0300

    apk teste #7

--- app/src/main/java/com/example/caio/socketstest/MainActivity.java
+++ app/src/main/java/com/example/caio/socketstest/MainActivity.java
@@ -7,32 +7,48 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnConexao;
+    private Button btnEnviar;
     private TextView txvRetornoSocket;
     private ProgressDialog load;
+    private EditText editText;
+    private ConexaoSocket cs = new ConexaoSocket();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.editText);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnEnviar.setClickable(false);
         txvRetornoSocket = (TextView) findViewById(R.id.txvRetornoSocket);
         btnConexao = (Button) findViewById(R.id.btnConexao);
         btnConexao.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(v == btnConexao){
                     Toast.makeText(MainActivity.this, "Conectando...", Toast.LENGTH_SHORT).show();
-                    ConexaoSocket cs = new ConexaoSocket();
                     cs.execute();
+                }
+            }
+        });
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnEnviar && editText.getText().toString() != null){
+                    cs.doInBackground(editText.getText().toString());
                 }
             }
         });
@@ -43,27 +59,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
     }
 
-    private class ConexaoSocket extends AsyncTask<Void, String, Void>{
+    private class ConexaoSocket extends AsyncTask<String, String, Void>{
 
         @Override
         protected void onPreExecute(){
             Log.i("AsyncTask", "Exibindo ProgressDialog na tela Thread: "+Thread.currentThread().getName());
+            btnConexao.setClickable(false);
+            btnEnviar.setClickable(true);
+            editText.setText(null);
             load = ProgressDialog.show(MainActivity.this, "Por favor aguarde...", "Conectando...");
+
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            Socket servidor = null;
+        protected Void doInBackground(String... args) {
+            Socket servidor;
             try {
                 servidor = new Socket("192.168.0.20", 3232);
                 try {
                     Scanner s = new Scanner(servidor.getInputStream());
-                    while(s.hasNextLine()){
+                    while (s.hasNextLine()) {
                         publishProgress(s.nextLine());
                     }
+
+                    if (args[0] != null) {
+                        PrintStream servidorSaida = new PrintStream(servidor.getOutputStream());
+                    servidorSaida.println(args[0]);
+                    }
+                    s.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                servidor.close();
             }catch (Exception e){
                 e.printStackTrace();
                 System.out.println("Erro: "+e.getMessage());
